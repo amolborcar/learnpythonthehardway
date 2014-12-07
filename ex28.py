2329c2a5959dfe4365a34a6b949e8d5635099adb
Amol Borcar <aborcar@gmail.com>
2014-12-07 14:38:22 -0800

    Finished Exercise 28.

---
+++ ex28.py
@@ -0,0 +1,23 @@
+'''
+1. True y
+2. False y
+3. False y
+4. True y
+5. True y
+6. False n
+7. False y
+8. True y
+9. False y
+10. False y
+11. True y
+12. False y
+13. True y
+14. False y
+15. False y
+16. True n
+17. True y
+18. True y
+19. False y
+20. False y
+'''
+
